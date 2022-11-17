df146c05ffdd01c5d804ec17c974bb4f511b2a5a
Alexeym <Alexeymun78@gmail.com>
2022-11-17 23:08:10 +0600

    Worked on form

---
+++ Bypass/wwwroot/js/bypass.js
@@ -0,0 +1,17 @@
+﻿function load() {
+    $.ajax('/api/sprevents')
+        .done((rez) => {
+            console.log(rez)
+            let node = $('select[name="event"]').get(0)
+
+            rez.forEach((i) => {
+                let item = document.createElement("option")
+                item.text = i.name
+                item.setAttribute("key", i.id)
+                node.appendChild(item)
+            })
+        })
+    $('input[name="event_date"]').datepicker({
+        dateFormat: 'dd.mm.yy',
+    })
+}
