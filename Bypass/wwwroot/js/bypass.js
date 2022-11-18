c32ba8c80c1ddde81e15fce52ca63abee9698b8e
fstrateg <Alexeym78@mail.ru>
2022-11-18 18:07:02 +0600

    Added finding a employee on edit page

--- Bypass/wwwroot/js/bypass.js
+++ Bypass/wwwroot/js/bypass.js
@@ -11,7 +11,33 @@
                 node.appendChild(item)
             })
         })
+    $.ajax('/api/spremploee').
+        done((rez) => {
+            let staff = []
+            rez.forEach((i) => staff.push({ label:i.name,value:i.id }))
+            $('#staff').autocomplete({
+                minLength: 2,
+                source: staff,
+                focus: function (event, ui) {
+                    $("#staff").val(ui.item.label);
+                    return false;
+                },
+                select: function (event, ui) {
+                    $("#staff").val(ui.item.label);
+                    $("#staff-vl").val(ui.item.value);
+                    return false;
+                },
+                change: function (event, ui) {
+                    if (ui.item == null) {
+                        $("#staff").val('')
+                        $("#staff-vl").val('');
+                    }
+                    return true;
+                },
+            })
+        })
     $('input[name="event_date"]').datepicker({
         dateFormat: 'dd.mm.yy',
     })
+
 }
